91fc6c52c105c98c6cce777d5eb8d0877fcd2ab7
Pierre Deveix <deveixpierre@gmail.com>
2022-04-22 22:59:16 +0200

    initializing User model

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,23 @@ const express = require('express');
 
 const app = express();
 
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://pierredeveix:panoramata@sidran83.5hbgs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
 app.use((req, res) => {
   res.json({ message: 'coucou'});
 });
