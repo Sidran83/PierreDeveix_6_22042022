51af7d936e8301a248813a91baecd5f55811c4fb
Pierre Deveix <deveixpierre@gmail.com>
2022-04-22 23:53:57 +0200

    adding token to user controller

--- backend/app.js
+++ backend/app.js
@@ -1,18 +1,16 @@
 const express = require('express');
-
-
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const userRoutes = require('./routes/user');
+
+const app = express();
 
 mongoose.connect('mongodb+srv://pierredeveix:panoramata@sidran83.5hbgs.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
   { useNewUrlParser: true,
     useUnifiedTopology: true })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
-
-const app = express();
-app.use(express.json());
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
@@ -21,6 +19,9 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use('api/auth', userRoutes);
+app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
