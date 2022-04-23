4444f25336788ab60808c532bb40fd3abca86ae8
Pierre Deveix <deveixpierre@gmail.com>
2022-04-23 10:22:02 +0200

    adding multer middleware to handle images

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const path = require('path');
 
 const userRoutes = require('./routes/user');
 
@@ -20,7 +21,8 @@ app.use((req, res, next) => {
 });
 
 app.use(bodyParser.json());
-// app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use('/api/auth', userRoutes);
 
